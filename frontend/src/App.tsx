27f8597ec2104d58bc543ff2706aa8c4382b80af
ritikeshyadav2020 <ritikeshyadav2020@gmail.com>
2024-12-29 11:33:29 +0000

    integrated tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="bg-red-600 text-4xl">  FlowTrack app</h1>
+      <Button>click me</Button>
+    </>
+  )
+}
+
+export default App
